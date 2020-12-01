99d2e7cd84fce39e32405a2019cede9bc980aaa9
Louis Leffler <louisleffler@gmail.com>
2020-12-01 15:30:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ end
 def get_japanese_emoticon(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
   emoticon_lib.keys.find do | key |
-     emoticon_lib[key]
+     emoticon_lib[key][:english]
     binding.pry
   end 
   japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
