863acd08eb21739c36d534248b737efae1bd908a
Louis Leffler <louisleffler@gmail.com>
2020-12-01 15:59:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ end
 def get_english_meaning(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
   english_emoticon = emoticon_lib.keys.find do | key |
-    emoticon_lib[key][:english][:japanese] == emoticon
+    emoticon_lib[key][:japanese] == emoticon
     #binding.pry
   end 
   english_emoticon ? emoticon_lib[english_emoticon][:english]: 'Sorry, that emoticon was not found'
